9c012632abaf1456d95eba9c19f70c229424e2fc
tusharsharma000 <toshu178@gmail.com>
2024-04-22 06:13:36 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,12 @@ const userRoute = require("./Routes/userRoute");
 const connectToMongo = require("./db/mongoConnection");
 const parser = require("cookie-parser");
 const { app,server, io } = require("./socket/socket");
+const path = require("path");
 
 
 dotEnv.config();
 const Port = process.env.Port || 5000;
+const __dirnames = path.resolve();
 
 app.use(express.json());
 app.use(parser());
@@ -20,6 +22,10 @@ app.use(parser());
 app.use("/api/auth", router);
 app.use("/api/messages", messageRoute);
 app.use("/api/users", userRoute);
+app.use(express.static(path.join(__dirnames, "/frontend/dist")));
+app.get("*",(req,res)=>{
+res.sendFile(path.join(__dirnames, "frontend", "dist", "index.html"));
+});
 
 
 server.listen(Port, ()=> { 
